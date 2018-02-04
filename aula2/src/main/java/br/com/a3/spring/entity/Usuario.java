508a292cbbf4ab381419265aa9f027a4ba802a9b
Felipe dos S. Assis <felipesntsassis@gmail.com>
2018-02-04 09:51:16 -0200

    Subindo aula 2.

---
+++ aula2/src/main/java/br/com/a3/spring/entity/Usuario.java
@@ -0,0 +1,14 @@
+package br.com.a3.spring.entity;
+
+public class Usuario {
+
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
